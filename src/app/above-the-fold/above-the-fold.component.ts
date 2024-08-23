54cf1a709512de11f3abb0768b49cbb4219ee557
Christian Frei <christian.frei.01@googlemail.com>
2024-08-23 23:16:08 +0200

    Implement footer

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialMediaLinksComponent } from '../shared/social-media-links/social-media-links.component';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [SocialMediaLinksComponent],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NameComponent } from '../name/name.component';
+import { SocialMediaLinksComponent } from '../social-media-links/social-media-links.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NameComponent],
+  imports: [NameComponent, SocialMediaLinksComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
