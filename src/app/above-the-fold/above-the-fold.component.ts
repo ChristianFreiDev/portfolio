b7a0587f84d3c5c9dd30b03b6f0c9208b63910f5
Christian Frei <christian.frei.01@googlemail.com>
2024-09-01 07:55:33 +0200

    Change main heading font size depending on language

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { SocialMediaLinksComponent } from '../shared/social-media-links/social-media-links.component';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, SocialMediaLinksComponent, TranslateModule],
+  imports: [RouterLink, RouterLinkActive, SocialMediaLinksComponent, TranslateModule, CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
-  constructor(private router: Router) { }
+
+  currentLanguage: string = this.translate.currentLang;
+
+  languageSubscription: Subscription;
+
+  constructor(private router: Router, private translate: TranslateService) { 
+    this.languageSubscription = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLanguage = event.lang;
+    })
+  }
+
+  ngOnDestroy() {
+    this.languageSubscription.unsubscribe();
+  }
 }
