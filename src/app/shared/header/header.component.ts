2d645bc452923ee40d7681b23e265c77d78f42b1
Christian Frei <christian.frei.01@googlemail.com>
2024-09-09 17:29:45 +0200

    Add burger menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IsActiveMatchOptions, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { NameComponent } from '../name/name.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -27,5 +27,45 @@ export class HeaderComponent {
   changeLanguage(language: string): void {
     this.translate.use(language);
     this.activeLanguage = language;
+  }
+
+  @ViewChild('headerBurgerMenuOverlay') overlay: any;
+  @ViewChild('headerBurgerMenuSvgAnimation1') menuSvgAnimation1: any;
+  @ViewChild('headerBurgerMenuSvgAnimation2') menuSvgAnimation2: any;
+  @ViewChild('headerBurgerMenuSvgAnimation3') menuSvgAnimation3: any;
+
+  isBurgerMenuOpen = false;
+
+  toggleBurgerMenu() {
+    this.isBurgerMenuOpen = !this.isBurgerMenuOpen;
+    if (this.isBurgerMenuOpen) {
+      this.openBurgerMenu();
+    } else {
+      this.closeBurgerMenu();
+    }
+  }
+
+  openBurgerMenu() {
+    document.body.classList.add('disable-scroll');
+    this.overlay.nativeElement.classList.add('show-burger-menu');
+    this.startAnimation();
+  }
+
+  closeBurgerMenu() {
+    this.isBurgerMenuOpen = false;
+    document.body.classList.remove('disable-scroll');
+    this.overlay.nativeElement.classList.remove('show-burger-menu');
+    this.startAnimation();
+  }
+
+  startAnimation() {
+    let anims = [this.menuSvgAnimation1.nativeElement, this.menuSvgAnimation2.nativeElement, this.menuSvgAnimation3.nativeElement];
+    anims.forEach(anim => {
+      anim.beginElement();
+      let from = anim.getAttribute('from');
+      anim.setAttribute('from', anim.getAttribute('to'));
+      anim.setAttribute('to', from);
+      console.log('Attributes changed');
+    })
   }
 }
