f89567b8d077377129d0e9bac100d589f8158ac5
Christian Frei <christian.frei.01@googlemail.com>
2024-09-12 20:52:30 +0200

    Improve responsiveness, refactor and add legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { UseScrollAnimationDirective } from '../directives/use-scroll-animation.directive';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, UseScrollAnimationDirective],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NameComponent } from '../name/name.component';
 import { SocialMediaLinksComponent } from '../social-media-links/social-media-links.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NameComponent, SocialMediaLinksComponent, TranslateModule],
+  imports: [RouterLink, NameComponent, SocialMediaLinksComponent, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
