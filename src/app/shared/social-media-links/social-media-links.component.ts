f96b9f9b74ab0d21cb296c2bf34f6786199ebef2
Christian Frei <christian.frei.01@googlemail.com>
2024-10-31 11:45:48 +0100

    Improve links

--- src/app/shared/social-media-links/social-media-links.component.ts
+++ src/app/shared/social-media-links/social-media-links.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-social-media-links',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './social-media-links.component.html',
   styleUrl: './social-media-links.component.scss'
 })
 export class SocialMediaLinksComponent {
-
+  @Input('isInFooter') isInFooter: boolean = false;
 }
