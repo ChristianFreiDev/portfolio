337f271cef3436a8892946afbb8a5651ac4e16b7
Christian Frei <christian.frei.01@googlemail.com>
2025-03-29 17:21:49 +0100

    Add workaround for Firefox and Brave on iOS, where svh does not work yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { HeaderComponent } from './shared/header/header.component';
@@ -9,13 +9,33 @@ import { FooterComponent } from './shared/footer/footer.component';
   standalone: true,
   imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+  isIOS = false;
 
   constructor(translate: TranslateService) {
+    this.isIOS = this.checkIOS();
     translate.setDefaultLang('de');
     translate.use('de');
+    this.setHeight(window.screen.height);
   }
+
+  checkIOS() {
+    const platform = (navigator as any).userAgentData?.platform || navigator.platform;
+    return platform.startsWith("iP") || platform.startsWith("Mac") && navigator.maxTouchPoints > 4;
+  }
+
+  setHeight(innerHeight: number) {
+    if (this.isIOS) {
+      document.documentElement.style.setProperty('--vh100', `${innerHeight}px`); 
+    }
+  }
+
+  @HostListener('window:resize', ['$event.target.screen.height'])
+  onResize(height: number) {
+    this.setHeight(height);
+  }
+
 }
